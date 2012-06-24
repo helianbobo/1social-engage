0e83ac8071ad5958c3f3a81b568ac1bdd5da85a9
Leegorous <leegorous@gmail.com>
2012-06-24 23:18:22 +0800

    load templates, toolbar and case

--- web-app/js/engage/post.js
+++ web-app/js/engage/post.js
@@ -9,13 +9,48 @@ $(document.body).ready(function() {
   }
 
   $(window).resize(updateListHeight)
-  updateListHeight()
+  // updateListHeight()
 
   var tmpl = $('.post-list li')
   for(var i = 0; i < 100; i++) $('.post-list').append(tmpl.clone())
 
   $('.post-item').delegate('.show-case', 'click', function() {
-    console.log('hi')
     $('.modal').modal()
+  })
+
+  function getTemplatePath(path) {
+    return path.indexOf('.') > 0 ? path : path + '.html'
+  }
+
+  function joinPath(s1, s2) {
+    s1 = s1.replace(/\/$/, '')
+    s2 = s2.replace(/^\//, '')
+    return s1 + '/' + s2;
+  }
+
+  function loadTemplate(el, fn) {
+    var url = joinPath($.rootPath, '/tmpl/' + getTemplatePath(el.attr('tmpl')))
+    console.log(url)
+    $.ajax({
+      url: url,
+      success: function(data) {
+        el.replaceWith(data)
+        appFrame.set({'templateLoaded': el.attr('class')})
+      }
+    })
+  }
+
+  var AppFrame = Backbone.Model.extend({})
+
+  var appFrame = new AppFrame()
+
+  appFrame.on('change:templateLoaded', function(model, className) {
+    var arr = className.split(' ')
+
+    if (_.find(arr, function(it) { return it === 'toolbar'})) updateListHeight()
+  })
+
+  $('[tmpl]').each(function(i, it) {
+    loadTemplate($(it))
   })
 })
