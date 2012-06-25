5ae4db444d57a7fe4bea15fac313fee31decf1cc
Leegorous <leegorous@gmail.com>
2012-06-25 20:05:59 +0800

    lazy loading the contents of case details tab

--- web-app/js/engage/case.js
+++ web-app/js/engage/case.js
@@ -1,3 +1,34 @@
+// Case
+
+// Models
+// -----
+engage.CaseApp = Backbone.View.extend(
+    {
+      initialize: function(options) {
+        var _t = this;
+        this.$('.case-details .nav-tabs a').one('show', function() {
+          var tab = $(this)
+
+          loadTemplates(_t.findPane(tab.attr('href')), function() { return true })
+        })
+
+        this.$('.case-details a:first').click();
+      }
+    , destroy: function() {
+        this.$el.remove()
+        delete this.el
+        delete this.$el
+      }
+    , findPane: function(id) {
+        var el = $(id)
+
+        if (el.length > 0) return el
+
+        return this.$('.case-details [name=' + id.split('#')[1] + ']:first')
+      }
+    }
+  )
+
 $(document.body).ready(function() {
   function repeat(ul, max) {
     var li = ul.find('li')
--- web-app/js/engage/post.js
+++ web-app/js/engage/post.js
@@ -1,11 +1,13 @@
+
+// Namespaces
+var engage = {}
+
 $(document.body).ready(function() {
   // constants
   var DEFAULT_TEMPLATE_FILTER = function(el) { return el.attr('tmpl-load-timing') != 'manual' }
 
   // Models
   // -----
-
-  var AppFrame = Backbone.Model.extend({})
 
   var ElementQueue = Backbone.Model.extend({
     push: function(obj) {
@@ -20,7 +22,7 @@ $(document.body).ready(function() {
   // Variables
   // -----
 
-  var appFrame = new AppFrame()
+  var appFrame = _.extend({}, Backbone.Events)
     , ids = 0
     , modalQueue = new ElementQueue({ max: 3 })
     , tmpls = {
@@ -47,7 +49,7 @@ $(document.body).ready(function() {
     return s1 + '/' + s2;
   }
 
-  function loadTemplate(el) {
+  function loadTemplate(el, callback) {
     var url = joinPath($.contextPath, '/tmpl/' + getTemplatePath(el.attr('tmpl')))
 
     $.ajax({
@@ -56,18 +58,23 @@ $(document.body).ready(function() {
         var newEl = $(data)
 
         el.replaceWith(newEl)
-        loadTemplates(newEl)
-        appFrame.set({'templateLoaded': el.attr('class')})
+        if ($.isFunction(callback)) callback(el, newEl)
       }
     })
   }
 
   function loadTemplates(el, filter) {
+    filter = filter || DEFAULT_TEMPLATE_FILTER
+    var callback = function(placeholder, el) {
+      loadTemplates(el, filter)
+      appFrame.trigger('templateLoaded', placeholder.attr('class'), el)
+    }
+
     el.find('[tmpl]').each(function(i, it) {
       var el = $(it)
-        , filter = filter || DEFAULT_TEMPLATE_FILTER
 
-      filter(el) && loadTemplate(el)
+      if (!filter) loadTemplate(el, callback)
+      filter(el) && loadTemplate(el, callback)
     })
   }
 
@@ -83,10 +90,11 @@ $(document.body).ready(function() {
     return el
   }
 
-  function repeat(ul) {
+  function repeat(ul, max) {
     var li = ul.find('li')
+      , max = max || 50
 
-    for (var i = 0; i < 100; i++) ul.append(li.clone())
+    for (var i = 0; i < max; i++) ul.append(li.clone())
   }
 
   function updateListHeight() {
@@ -96,15 +104,29 @@ $(document.body).ready(function() {
   // Event listeners
   // -----
 
-  appFrame.on('change:templateLoaded', function(model, className) {
+  appFrame.on('templateLoaded', function(className, el) {
     var arr = className.split(' ')
 
-    if (_.find(arr, function(it) { return it === 'toolbar'})) updateListHeight()
+    function has(obj) {
+      return _.find(arr, function(it) { return it === obj })
+    }
+
+    if (has('toolbar')) updateListHeight()
+    if (has('app-case')) {
+      var obj = new engage.CaseApp({el: el})
+      el.parents('.modal').on('remove', function() {
+        obj.destroy()
+        obj = null
+      })
+    }
   })
 
   modalQueue.on('change:queue', function(model, queue) {
     if (queue.length > model.get('max')) {
-      $('#' + queue.shift()).remove()
+      var el = $('#' + queue.shift())
+      
+      el.trigger('remove')
+      el.remove()
       this.set('queue', _.map(queue, function(it) { return it }))
     }
   })
@@ -130,6 +152,7 @@ $(document.body).ready(function() {
   $(window).resize(updateListHeight)
   updateListHeight()
   loadTemplates($('#page'))
+  window.loadTemplates = loadTemplates
 
   // TODO remove it after test
   repeat($('.post-list'))
