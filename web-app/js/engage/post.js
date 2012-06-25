ab9018d9a6ff12685958e8b90557c4b703fb5e68
Leegorous <leegorous@gmail.com>
2012-06-25 11:05:00 +0800

    use ElementQueue to manage modals

--- web-app/js/engage/post.js
+++ web-app/js/engage/post.js
@@ -1,7 +1,33 @@
 $(document.body).ready(function() {
+  // constants
+  var DEFAULT_TEMPLATE_FILTER = function(el) { return el.attr('tmpl-load-timing') != 'manual' }
+
+  // models
   var AppFrame = Backbone.Model.extend({})
 
+  var ElementQueue = Backbone.Model.extend({
+    push: function(obj) {
+      var arr = []
+
+      this.get('queue') && arr.push.apply(arr, this.get('queue'))
+      arr.push(obj)
+      this.set('queue', arr)
+    }
+  })
+
+  // variables
   var appFrame = new AppFrame()
+    , ids = 0
+    , modalQueue = new ElementQueue({ max: 3 })
+    , tmpls = {
+        'app-case-modal': $('.app-case-modal').remove()
+      , get: function(name) {
+          var el = this[name]
+
+          if (!el) throw 'could not get template named ' + name
+          return el.clone()
+        }
+      }
 
   function getListHeight() {
     return $(window).height() - $('.app-tabs').outerHeight() - $('.toolbar').outerHeight() - 30
@@ -29,6 +55,27 @@ $(document.body).ready(function() {
     })
   }
 
+  function loadTemplates(el, filter) {
+    el.find('[tmpl]').each(function(i, it) {
+      var el = $(it)
+        , filter = filter || DEFAULT_TEMPLATE_FILTER
+
+      filter(el) && loadTemplate(el)
+    })
+  }
+
+  function newId(prefix, suffix) {
+    return (prefix ? prefix : '') + (++ids) + (suffix ? suffix : '')
+  }
+
+  function prepareModal(name) {
+    var el = tmpls.get(name)
+
+    el.attr('id', newId('modal-'))
+    $(document.body).append(el)
+    return el
+  }
+
   function repeat(ul) {
     var li = ul.find('li')
 
@@ -39,21 +86,44 @@ $(document.body).ready(function() {
     $('.post-list-wrap').height(getListHeight())
   }
 
+  // Event listeners
+  // -----
+
   appFrame.on('change:templateLoaded', function(model, className) {
     var arr = className.split(' ')
 
     if (_.find(arr, function(it) { return it === 'toolbar'})) updateListHeight()
   })
 
-  $('.post-item').delegate('.show-case', 'click', function() {
-    $('.modal').modal()
+  modalQueue.on('change:queue', function(model, queue) {
+    if (queue.length > model.get('max')) {
+      $('#' + queue.shift()).remove()
+      this.set('queue', _.map(queue, function(it) { return it }))
+    }
   })
+
+  $('.post-list').delegate('.show-case', 'click', function() {
+    var attrName = 'modal-id'
+      , btn = $(this)
+      , el = $('#' + btn.attr(attrName))
+
+    if (el.length === 0) {
+      el = prepareModal('app-case-modal')
+      btn.attr(attrName, el.attr('id'))
+      modalQueue.push(el.attr('id'))
+      el.on('show', _.once(function() { loadTemplates(el) }))
+    }
+    
+    el.modal()
+  })
+
+  // Page setup
+  // -----
 
   $(window).resize(updateListHeight)
   updateListHeight()
-  repeat($('.post-list'))
+  loadTemplates($('#page'))
 
-  $('[tmpl]').each(function(i, it) {
-    loadTemplate($(it))
-  })
+  // TODO remove it after test
+  repeat($('.post-list'))
 })
