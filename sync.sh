04a4e096b19e0d2ea087190f8d03506a8874d4ac
helianbobo <541116>
2012-06-29 14:17:41 +0800

    dummy API

---
+++ sync.sh
@@ -0,0 +1,11 @@
+UI_PROJECT_HOME=../UI
+
+cp -rf  web-app/images/* $UI_PROJECT_HOME/web-app/engage/images/
+cp -rf  web-app/less/* $UI_PROJECT_HOME/web-app/engage/less/
+cp -rf  web-app/js/* $UI_PROJECT_HOME/web-app/engage/js/
+cp -rf  web-app/font/* $UI_PROJECT_HOME/web-app/engage/font/
+
+cp -rf  web-app/tmpl/* $UI_PROJECT_HOME/web-app/tmpl/
+
+cp grails-app/views/socialEngage/cases.gsp $UI_PROJECT_HOME/grails-app/views/socialEngage/cases.gsp
+cp grails-app/views/socialEngage/posts.gsp $UI_PROJECT_HOME/grails-app/views/socialEngage/posts.gsp
