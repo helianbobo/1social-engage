d3a2c48d1e916c85841248f233f3625c9054d3bc
Leegorous <leegorous@gmail.com>
2012-07-15 19:57:51 +0800

    #31 tooltip for case sort

--- web-app/js/engage/cases.js
+++ web-app/js/engage/cases.js
@@ -241,6 +241,10 @@
     }
   )
   
+  function resetTitle(str) {
+    return str.replace(/^sort by /i, '').replace(/ in (ascending|descending) order$/i, '')
+  }
+
   Toolbar.SortActions = Backbone.View.extend(
     {
       events: { 'click a': 'sort' }
@@ -250,7 +254,7 @@
           var el = $(it)
 
           el.removeClass('asc desc')
-          el.attr('title', el.attr('title').replace(/ (asc|desc)$/i, ''))
+          el.attr('title', 'sort by ' + resetTitle(el.attr('title')))
         })
       }
 
@@ -263,9 +267,11 @@
       }
 
     , updateStatus: function(el, flag) {
+        var m = { asc: 'ascending', desc: 'descending' }
+        
         this.reset()
         el.addClass(flag)
-        el.attr('title', el.attr('title') + ' ' + flag.toUpperCase())
+        el.attr('title', resetTitle(el.attr('title')) + ' in ' + m[flag] + ' order')
         this.collection.params.set(
             { 
               sort: el.attr('href').split('#')[1]
