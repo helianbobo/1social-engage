59ccca0295171a3f7ce542bbb6e59bd7a92695d2
Leegorous <leegorous@gmail.com>
2012-07-29 18:28:38 +0800

    active datepicker & inner tab

--- web-app/js/engage/overview.js
+++ web-app/js/engage/overview.js
@@ -1,19 +1,44 @@
 ;define(['engage', 'jquery-ui.datepicker', 'lib/highcharts/brandtology'], function(engage) {
 
-  var OverviewItem = Backbone.View.extend({
+  var Overview = Backbone.View.extend({
+        initialize: function(options) {
+          this.$el.find('.date-dropdown input').datepicker({
+            dateFormat: 'dd MM yy'
+          , changeMonth: true
+          , numberOfMonths: 1
+          });
 
-  })
+          this.$el.delegate('.date-dropdown', 'click', function() {
+            $(this).find('input').datepicker('show')
+          })
 
-  var templateListeners = {
-    'engage-overview:one': function(el, placeholder) {
-      
-    }
-  }
+          this.model.on('sync', this.render, this)
+          if (this.model.length > 0) {
+            this.render(this.model)
+          }
+        }
+
+      , render: function(model) {
+          
+        }
+      })
+
+  var assets = new engage.model.Assets()
+    , templateListeners = {
+        'engage-overview:one': function(el, placeholder) {
+          new Overview({
+            model: assets
+          , el: el
+          })
+        }
+      }
   // Page setup
   // -----
   $(document.body).ready(function() {
     //$(window).resize(updateListHeight)
     engage.showAjax()
+
+    assets.fetch()
   
     tmplLoader.addListeners(templateListeners)
     tmplLoader.load($('#page'))
